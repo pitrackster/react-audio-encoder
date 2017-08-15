1b167bb436e245e77a9fa09e4d178593ac2f8c20
Guillou Patrick <pitrackster@gmail.com>
2017-08-15 18:36:18 +0200

    update php script

--- script.php
+++ script.php
@@ -5,37 +5,41 @@ $samplerate = isset($_POST['samplerate']) ? intval($_POST['samplerate']) : 44100
 $bitrate = isset($_POST['bitrate']) ? intval($_POST['bitrate']) : 256;
 $container =  isset($_POST['container']) ? $_POST['container'] : 'ogg';
 
-var_dump($_POST);
-var_dump($_FILES);
+// var_dump($_POST);
+// var_dump($_FILES);
 
 if (isset($_FILES) && count($_FILES) > 0) {
-    echo 'yope';
+
     for($i = 0; $i < count($_FILES); $i++) {
-        $fileName = $_FILES[$i]['name'];
-        echo 'yayay ' .$fileName;
-        move_uploaded_file($_FILES[$i]['tmp_name'], "uploaded/" . $fileName);
+        move_uploaded_file($_FILES[$i]['tmp_name'], "uploaded/" . $_FILES[$i]['name']);
+    }
+    // exit;
 
-        $path_parts = pathinfo($fileName);
-        // audio file name = video file name without extension
-        $audioFileName =  $path_parts['filename'];
+    // read all files in uploaded dir (do not keep hodden files (.gitkeep for example))
+    $files = preg_grep('/^([^.])/', scandir('uploaded'));
 
-        echo 'yayay audio ' .$audioFileName;
+    //print_r($files);
 
-        $cmd = 'ffmpeg -i "uploaded/' .$fileName. '" "encoded/' . $audioFileName . '.' . $container.'"';
-        echo 'cmd ' .$cmd;
-
+    foreach($files as $file){
+        $path_parts = pathinfo($file);
+        // audio file name = video file name without original extension but with given container ext
+        $encodedFileName =  $path_parts['filename'] . '.' . $container;
+        $cmd = 'ffmpeg -i "uploaded/' .$file. '" -ar ' . $samplerate . ' -ab '.$bitrate.'k "encoded/' . $encodedFileName . '"';
         exec($cmd);
     }
-    exit;
+
+    // remove all uploaded files
+
+
+    // zip encoded folder
+
+
+    // read zip content as a response
+
+    
 }
 
-/*
-if (isset($_FILES['myFile'])) {
-    // Example:
-    move_uploaded_file($_FILES['myFile']['tmp_name'], "" . $_FILES['myFile']['name']);
-    exit;
-}
-*/
+
 die;
 
 
